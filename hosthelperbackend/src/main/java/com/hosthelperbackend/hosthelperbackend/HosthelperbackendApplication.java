535dced572a7fc9635cbb9420f55b6f5b0a021df
YLudvig <yslu24ne@student.ju.se>
2026-04-06 15:18:03 +0200

    Start of backend

---
+++ hosthelperbackend/src/main/java/com/hosthelperbackend/hosthelperbackend/HosthelperbackendApplication.java
@@ -0,0 +1,13 @@
+package com.hosthelperbackend.hosthelperbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HosthelperbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HosthelperbackendApplication.class, args);
+	}
+
+}
---
+++ hosthelperbackend/src/test/java/com/hosthelperbackend/hosthelperbackend/HosthelperbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hosthelperbackend.hosthelperbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HosthelperbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
