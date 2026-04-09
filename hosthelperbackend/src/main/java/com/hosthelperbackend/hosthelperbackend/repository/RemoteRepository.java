9348693bf83386bb2026ffccde8448e3aecb02cb
YLudvig <yslu24ne@student.ju.se>
2026-04-09 17:27:49 +0200

    Repo method to get all remotes for profile/user

--- hosthelperbackend/src/main/java/com/hosthelperbackend/hosthelperbackend/repository/RemoteRepository.java
+++ hosthelperbackend/src/main/java/com/hosthelperbackend/hosthelperbackend/repository/RemoteRepository.java
@@ -2,9 +2,18 @@ package com.hosthelperbackend.hosthelperbackend.repository;
 
 import com.hosthelperbackend.hosthelperbackend.model.Remote;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface RemoteRepository extends JpaRepository<Remote, UUID> {
+
+    @Query(value = """
+            SELECT * 
+            FROM remote
+            WHERE user_id = :userId
+    """, nativeQuery = true)
+    List<Remote> findAllRemotesByUserID(UUID userId);
 
 }
